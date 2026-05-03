19dce18ebc176b22ec0b18113fbfb532c623922c
tanvirahmedrume <tstanvir37180^@gmail.com>
2026-05-03 22:32:21 +0600

    setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center min-h-screen">
+        <h1 className="text-4xl font-bold mb-4">Welcome to Bistro Client!</h1>
+        <p className="text-lg text-gray-600">This is a simple React application using Tailwind CSS and DaisyUI.</p>
+        <button className="btn btn-primary">Get Started</button>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+  return (
+    <div>
+        <h1 className="text-xl font-bold mb-4">Welcome to Bistro Client!</h1>
+    </div>
+  )
+}
+
+export default Home
