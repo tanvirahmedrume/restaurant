cb283f5b1d92c3ed99020e74fad92ecec4f67a98
tanvirahmedrume <tstanvir371806@gmail.com>
2026-05-04 22:58:34 +0600

    added

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,6 +1,5 @@
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
-
 import img1 from "../../../assets/home/01.jpg";
 import img2 from "../../../assets/home/02.jpg";
 import img3 from "../../../assets/home/03.png";
