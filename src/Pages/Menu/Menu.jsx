8bd9daf389216eaaba9e3e67fe8cd57337414034
tanvirahmedrume <tstanvir371806@gmail.com>
2026-05-09 22:47:06 +0600

    added menu images

--- src/Pages/Menu/Menu.jsx
+++ src/Pages/Menu/Menu.jsx
@@ -1,10 +1,14 @@
 import {Helmet} from "react-helmet-async";
+import Cover from "../Shared/Cover/Cover";
+import menuImg from '../../assets/menu/banner3.jpg'
+
 const Menu = () => {
   return (
     <div>
         <Helmet>
             <title>Menu | Bistro Boss</title>
         </Helmet>
+        <Cover img={menuImg}></Cover>
         <h2 className="text-3xl text-center">This is Menu page</h2>
     </div>
   )
