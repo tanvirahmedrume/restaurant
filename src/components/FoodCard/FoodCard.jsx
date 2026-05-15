ace037301de14bc9ed39270afcec8422645b5630
tanvirahmedrume <tstanvir371806@gmail.com>
2026-05-15 23:47:13 +0600

    added something

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -1,12 +1,29 @@
-
 const FoodCard = () => {
   return (
     <div>
       <h1>FoodCard</h1>
       <p>This is a simple food card component.</p>
-      
-    </div>
-  )
-}
 
-export default FoodCard
+      <div className="card bg-base-100 w-96 shadow-sm">
+        <figure>
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+            alt="Shoes"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">Card Title</h2>
+          <p>
+            A card component has a figure, a body part, and inside body there
+            are title and actions parts
+          </p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FoodCard;
